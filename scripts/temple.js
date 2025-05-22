7313b798f181629950b0fe5e3e292e737b5ccd48
baezbenjamin <baezbenjamin03@gmail.com>
2025-05-21 23:36:36 -0300

    Temple Album Finished

--- scripts/temple.js
+++ scripts/temple.js
@@ -7,3 +7,11 @@ year.innerHTML = `<span>${today.getFullYear()}</span>`;
 const mod = document.querySelector("#lastModified");
 
 mod.innerHTML = `<p>Last Modification: ${document.lastModified}</p>`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
