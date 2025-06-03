fec8b327c45fc9aba5d9bdc728bbe838552cd1ca
baezbenjamin <baezbenjamin03@gmail.com>
2025-06-03 00:47:24 -0300

    JavaScript for Wind Chill Calculation Added

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,23 @@ year.innerHTML = `<span>${today.getFullYear()}</span>`;
 const mod = document.querySelector("#lastModified");
 
 mod.innerHTML = `<p>Last Modification: ${document.lastModified}</p>`;
+
+const windChill = document.querySelector('#windchill');
+
+let temparature = 15;
+let windSpeed = 6;
+
+function calculateWindChill(temparature, windSpeed) {
+    let windChillResult = 0;
+    windChillResult = 13.12 + (0.6215 * temparature) - (11.37 * (windSpeed ** 0.16)) + (0.3965 * temparature * (windSpeed ** 0.16));
+    return windChillResult;
+}
+
+let finalWindChill = calculateWindChill(temparature, windSpeed);
+
+if (temparature <= 10 && windSpeed > 4.8) {
+    windChill.innerHTML = `<span>${finalWindChill.toFixed(2)}°C</span>`
+}
+else {
+    windChill.innerHTML = `<span>N/A</span>`
+}
